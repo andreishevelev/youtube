0f905bb3a2471c8d0548687abc248965c0c33e6c
andreishevelev <shevelev.andrew@gmail.com>
2022-12-11 19:48:19 -0600

    video name changed

--- test/rewatch.js
+++ test/rewatch.js
@@ -71,7 +71,7 @@ describe(`Can watch video again and again and ...`, function () {
       await driver.sleep(1000);
       await searchButton.click();
 
-      let channel = await waitLV(By.xpath(`//*[.='veselovka']`), defTimeout);
+      let channel = await driver.wait(until.elementLocated(By.xpath(`//*[.='veselovka']`)), defTimeout);
       await actions.move({ origin: channel }).perform();
       await driver.sleep(1000);
       await channel.click();
